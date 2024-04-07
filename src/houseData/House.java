9c1f9f91c24521f6962a49d369aff7b1f534a930
Emil Sunny <emilsunny111@gmail.com>
2024-04-07 16:07:05 -0600

    my commit

---
+++ src/houseData/House.java
@@ -0,0 +1,36 @@
+package houseData;
+
+public class House {
+	int age;
+	double hieght;
+	String name;
+
+	boolean isAccessGranted;
+
+	void entryForWaterCanyon() {
+		isAccessGranted = ((age >= 13) && (hieght >= 4.5));
+		if (isAccessGranted)
+			System.out.println("Water Canyon : Access Granted ");
+		else {
+			System.out.println("Water Canyon : Not permitted");
+		}
+	}
+
+	void enteryForRedBaraon() {
+		isAccessGranted = ((age >= 15) && (hieght >= 5.5));
+		if (isAccessGranted)
+			System.out.println("Red Baron : Access Granted");
+		else {
+			System.out.println("Red Baron : Not permitted");
+		}
+	}
+
+	void enteryForswingTime() {
+		isAccessGranted = ((age >= 18) && (hieght >= 3.5));
+		if (isAccessGranted)
+			System.out.println("Swing Time : Access Granted");
+		else {
+			System.out.println("Swing Time : Not permitted");
+		}
+	}
+}
