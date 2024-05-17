6c880cc843fed7f421ee1f489b969074a10eda10
Emil Sunny <emilsunny111@gmail.com>
2024-05-16 20:35:23 -0600

    my commit

---
+++ src/test/MyClass.java
@@ -0,0 +1,11 @@
+package test;
+
+import rideOccupancy.RideOccupancyLimits;
+
+public class MyClass {
+	public static void main(String[] args) {
+		RideOccupancyLimits ro=new RideOccupancyLimits();
+		
+		ro.peopleAllowedInRide();
+	}
+}
